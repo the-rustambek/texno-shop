e414997ee3013a80b1e6028313853723c282b6c2
rustambek-Mamadaliyev0301 <rustambek0301@gmail.com>
2022-11-05 10:30:03 +0500

    Add swiper icons

--- src/Pages/ProductsAboutPage/components/SwiperProduct.jsx
+++ src/Pages/ProductsAboutPage/components/SwiperProduct.jsx
@@ -6,15 +6,32 @@ import "swiper/css/pagination";
 import "swiper/css/navigation";
 import phone from "../../../Assets/Svg/phone.svg"
 import phone1 from "../../../Assets/Svg/phone1.svg"
-
+import refresh from "../../../Assets/Svg/refresh.svg"
+import price from "../../../Assets/Svg/price.svg"
+import skidka from "../../../Assets/Svg/skidka.svg"
+import skidkagreen from "../../../Assets/Svg/skidkagreen.svg"
 import "../ProductsAboutPage.css";
-
  
 import {   Pagination, Navigation } from "swiper";
 
 export default function SwiperProduct() {
   return (
     <div className="swiper-product">
+        <ul className="swiper-list">
+          <li className="swiper-item">
+            <img src={refresh} alt="refresh" className="swiper-icon" />
+          </li>
+          <li className="swiper-item  swiper-price">
+            <img src={price} alt="refresh" className="swiper-icon" />
+          </li>
+          <li className="swiper-item swiper-skidka">
+            <img src={skidka} alt="refresh" className="swiper-icon" />
+          </li>
+          <li className="swiper-item swiper-skidkagreen">
+            <img src={skidkagreen} alt="refresh" className="swiper-icon" />
+          </li>
+        </ul>
+      
       <Swiper
         style={{
            "--swiper-pagination-color": "red",
